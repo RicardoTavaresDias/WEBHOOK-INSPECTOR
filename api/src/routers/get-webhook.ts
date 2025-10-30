4164894be4f978bdb174af9a2fbc1013466466f8
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-10-30 11:17:22 -0300

    version 1.0.1

---
+++ api/src/routers/get-webhook.ts
@@ -0,0 +1,40 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { createSelectSchema } from "drizzle-zod"
+import { webhooks } from '@/db/schema'
+import { db } from '@/db'
+import { eq } from 'drizzle-orm'
+
+export const getWebhook: FastifyPluginAsyncZod = async (app) => {
+  app.get(
+    '/api/webhooks/:id',
+    {
+      schema: {
+        summary: 'Get a specific webhook by ID',
+        tags: ['Webhooks'],
+        params: z.object({
+          id: z.uuidv7()
+        }),
+        response: {
+          200: createSelectSchema(webhooks),
+          404: z.object({ message: z.string() })
+        },
+      },
+    },
+    async (request, replay) => {
+      const { id } = request.params
+
+      const result = await db
+        .select()
+        .from(webhooks)
+        .where(eq(webhooks.id, id))
+        .limit(1)
+
+      if (result.length === 0) {
+        return replay.status(404).send({ message: 'Webhook not found.' })
+      }
+
+      return replay.send(result[0])
+    },
+  )
+}
